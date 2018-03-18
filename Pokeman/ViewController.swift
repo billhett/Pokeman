862f1afd717b038234072e066d559e5c8ebfbc01
William Hettinger <bill.hettinger@gmail.com>
2018-03-18 13:35:36 +0100

    add centering button

--- Pokeman/ViewController.swift
+++ Pokeman/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var updateCount = 0
+    var manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("Ready to go!")
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("We made it")
+        if updateCount < 3 {
+            let region = MKCoordinateRegionMakeWithDistance((manager.location?.coordinate)!, 400,400)
+            mapView.setRegion(region, animated: false)
+            updateCount += 1
+        } else {
+            manager.stopUpdatingLocation()
+        }
+    }
+    
+    @IBAction func centerTapped(_ sender: Any) {
+        if let coord = manager.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(coord, 400,400)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
 }
 
