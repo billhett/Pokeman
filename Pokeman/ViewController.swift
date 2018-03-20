228cea33a5d976782210582387686902a34fcda2
William Hettinger <bill.hettinger@gmail.com>
2018-03-20 21:24:06 +0100

    completed app - caught pokemons are deleted, out of range pokemon get an alert saying they are out of range

--- Pokeman/ViewController.swift
+++ Pokeman/ViewController.swift
@@ -23,34 +23,45 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
         manager.delegate = self
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
-            print("Ready to go!")
-            mapView.delegate = self
-            mapView.showsUserLocation = true
-            manager.startUpdatingLocation()
-            
-            Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
-                //spawn Pokemon every 5 seconds
-                print("timer")
-                
-                if let coord = self.manager.location?.coordinate {
-                    let pokemon = self.pokemons[Int(arc4random_uniform(UInt32(self.pokemons.count)))]
-                    let anno = PokeAnnotation(coord: coord, pokemon: pokemon)
-                    //let region = MKCoordinateRegionMakeWithDistance(coord, 200,200)
-                    //self.mapView.setRegion(region, animated: true)
-                    anno.coordinate = coord
-                    let randoLat = (Double(arc4random_uniform(200))-100)/50000.0
-                    let randoLon = (Double(arc4random_uniform(200)) - 100)/50000
-                    anno.coordinate.latitude += randoLat
-                    anno.coordinate.longitude += randoLon
-                    self.mapView.addAnnotation(anno)
-                }
-                
-            })
+            setup()
         } else {
             manager.requestWhenInUseAuthorization()
         }
         
         
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            setup()
+        }
+    }
+    
+    
+    func setup() {
+        print("Ready to go!")
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        manager.startUpdatingLocation()
+        
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
+            //spawn Pokemon every 5 seconds
+            print("timer")
+            
+            if let coord = self.manager.location?.coordinate {
+                let pokemon = self.pokemons[Int(arc4random_uniform(UInt32(self.pokemons.count)))]
+                let anno = PokeAnnotation(coord: coord, pokemon: pokemon)
+                //let region = MKCoordinateRegionMakeWithDistance(coord, 200,200)
+                //self.mapView.setRegion(region, animated: true)
+                anno.coordinate = coord
+                let randoLat = (Double(arc4random_uniform(200))-100)/50000.0
+                let randoLon = (Double(arc4random_uniform(200)) - 100)/50000
+                anno.coordinate.latitude += randoLat
+                anno.coordinate.longitude += randoLon
+                self.mapView.addAnnotation(anno)
+            }
+            
+        })
     }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
@@ -69,11 +80,56 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         let pokemon = (annotation as! PokeAnnotation).pokemon
         
         annoView.image = UIImage(named: pokemon.imageName!)
+        print("Spawing a \(pokemon.name!)")
         var frame = annoView.frame
         frame.size.height = 50
         frame.size.width = 50
         annoView.frame = frame
         return annoView
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        mapView.deselectAnnotation(view.annotation!, animated: true)
+        if view.annotation is MKUserLocation {
+            return
+        }
+        
+        let region = MKCoordinateRegionMakeWithDistance(view.annotation!.coordinate, 200,200)
+        mapView.setRegion(region, animated: true)
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { (timer) in
+            if let coord = self.manager.location?.coordinate{
+                print("annotation tapped")
+                let pokemon = (view.annotation as! PokeAnnotation).pokemon
+                if MKMapRectContainsPoint(mapView.visibleMapRect, MKMapPointForCoordinate(coord)) {
+                    
+                    print("can catch pokemon")
+                    let alertVC = UIAlertController(title: "Awesome!", message: "You caught the \(pokemon.name!) - you are a Pokemon Master!.", preferredStyle: .alert)
+                    let OKaction = UIAlertAction(title: "OK", style: .default, handler: { (action) in
+                        
+                    })
+                    alertVC.addAction(OKaction)
+                    let PokedexAction = UIAlertAction(title: "Pokedex", style: .default, handler: { (action) in
+                        self.performSegue(withIdentifier: "pokedexSeque", sender: nil)
+                        
+                        
+                    })
+                    alertVC.addAction(PokedexAction)
+                    self.present(alertVC, animated: true, completion: nil)
+                    pokemon.caught = true
+                    (UIApplication.shared.delegate as! AppDelegate).saveContext()
+                    mapView.removeAnnotation(view.annotation!)
+                }else {
+                    print("Pokemon is too far away")
+                    let alertVC = UIAlertController(title: "Uh-Oh!", message: "You are too far away to catch the \(pokemon.name!) - move closer to it.", preferredStyle: .alert)
+                    let OKaction = UIAlertAction(title: "OK", style: .default, handler: { (action) in
+                        
+                    })
+                    alertVC.addAction(OKaction)
+                    self.present(alertVC, animated: true, completion: nil)
+                }
+            }
+            
+        }
     }
     
     override func didReceiveMemoryWarning() {
